9ee614f8c75660c742ea3bc3e6ee4f2631e9ab51
Ahmed Ishag <ahmedishag96zz@gmail.com>
2024-10-09 16:27:41 +0200

    updating my webhook whats3.2

--- service.go
+++ service.go
@@ -34,6 +34,7 @@ func HandlePostRequest(r *http.Request, w http.ResponseWriter) {
 	var notifyReq NotificationReq
 	data, _ := json.Marshal(req)
 	json.Unmarshal(data, &notifyReq)
+	fmt.Println("My notification", notifyReq)
 	obj := notifyReq.Entry.Changes[0].Value.Messages[0]
 	message := map[string]interface{}{"sender": obj.From,
 		"message_id": obj.ID, "text": obj.Text.Body}
